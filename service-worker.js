054d3dc44793fddefe594ff19cd7db7533a8d8b2
Mark Ezdahmad <ezdahmad@gmail.com>
2025-08-03 03:56:32 +0400

    progress

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'ezdahmad-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/privacy-policy.html',
+  '/apple-touch-icon.png',
+  '/manifest.json'
+];
+
+// Install SW and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Update cache on activation
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+                  .map(name => caches.delete(name))
+      );
+    })
+  );
+});
